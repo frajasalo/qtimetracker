c07c16d7a9295adcfb4b6149f3bd3797b3ad7664
Frank Bergmann <github@frajasalo.de>
2014-04-03 22:04:28 +0200

    Some unit tests for the storage options class
    Signed-off-by: Frank Bergmann <github@frajasalo.de>

---
+++ tests/IStorageOptions_UnitTest/IStorageOptions_UnitTest.pro
@@ -0,0 +1,19 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-03-26T17:57:58
+#
+#-------------------------------------------------
+
+QT       += testlib
+
+QT       -= gui
+
+TARGET = tst_istorageoptions_unittest
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += tst_istorageoptions_unittest.cpp ../../storageoptions.cpp
+DEFINES += SRCDIR=\\\"$$PWD/\\\"
---
+++ tests/IStorageOptions_UnitTest/tst_istorageoptions_unittest.cpp
@@ -0,0 +1,87 @@
+#include <QString>
+#include <QtTest>
+
+#include "../../storageoptions.h"
+
+class IStorageOptions_UnitTest : public QObject
+{
+    Q_OBJECT
+
+public:
+    IStorageOptions_UnitTest();
+
+private Q_SLOTS:
+    void testCase_AddOption();
+    void testCase_GetOptionValue();
+    void testCase_SetOptionValue();
+};
+
+IStorageOptions_UnitTest::IStorageOptions_UnitTest() {}
+
+void IStorageOptions_UnitTest::testCase_AddOption() {
+
+    IStorageOptions uut;
+    QCOMPARE(0, (int)uut.GetOptions().size());
+
+    StoreOption_t opt1;
+    opt1.name = "test";
+    opt1.value = "test";
+    QCOMPARE(true, uut.AddOption(opt1));
+    QCOMPARE(1, (int)uut.GetOptions().size());
+
+    StoreOption_t opt2;
+    opt2.name = "tset";
+    opt2.value = "tset";
+    QCOMPARE(true, uut.AddOption(opt2));
+    QCOMPARE(2, (int)uut.GetOptions().size());
+
+    QCOMPARE(false, uut.AddOption(opt1));
+    QCOMPARE(2, (int)uut.GetOptions().size());
+    opt2.name = "test";
+    QCOMPARE(false, uut.AddOption(opt2));
+    QCOMPARE(2, (int)uut.GetOptions().size());
+}
+
+void IStorageOptions_UnitTest::testCase_GetOptionValue() {
+
+    IStorageOptions uut;
+    StoreOption_t opt;
+    opt.name = "aaa";
+    opt.value = "aaa";
+    QCOMPARE(true, uut.AddOption(opt));
+    opt.name = "bbb";
+    opt.value = "bbb";
+    QCOMPARE(true, uut.AddOption(opt));
+    opt.name = "ccc";
+    opt.value = "ccc";
+    QCOMPARE(true, uut.AddOption(opt));
+
+    QCOMPARE(std::string("bbb"), uut.GetOptionValue("bbb"));
+    QCOMPARE(std::string("aaa"), uut.GetOptionValue("aaa"));
+    QCOMPARE(std::string(), uut.GetOptionValue("abc"));
+}
+
+void IStorageOptions_UnitTest::testCase_SetOptionValue() {
+
+    IStorageOptions uut;
+    StoreOption_t opt;
+    opt.name = "aaa";
+    opt.value = "aaa";
+    QCOMPARE(true, uut.AddOption(opt));
+    opt.name = "bbb";
+    opt.value = "bbb";
+    QCOMPARE(true, uut.AddOption(opt));
+    opt.name = "ccc";
+    opt.value = "ccc";
+    QCOMPARE(true, uut.AddOption(opt));
+
+    QCOMPARE(std::string("bbb"), uut.GetOptionValue("bbb"));
+    QCOMPARE(std::string("xyz"), uut.SetOptionValue("bbb", "xyz"));
+    QCOMPARE(std::string("xyz"), uut.GetOptionValue("bbb"));
+    QCOMPARE(std::string("aaa"), uut.GetOptionValue("aaa"));
+    QCOMPARE(std::string(), uut.GetOptionValue("abc"));
+}
+
+QTEST_APPLESS_MAIN(IStorageOptions_UnitTest)
+
+#include "tst_istorageoptions_unittest.moc"
